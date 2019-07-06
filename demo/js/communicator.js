7d9b12d509f104045c162d2d94307018cb5c8ee4
Praveen Penumaka <praveen.penumaka@go-jek.com>
2019-07-06 16:41:41 +0530

    New demo added

---
+++ demo/js/communicator.js
@@ -0,0 +1,318 @@
+function J(qString){
+	this.elements = [];
+	if(qString.indexOf(".")===0){
+		var className = qString.substring(1,qString.length)
+		this.elements = document.getElementsByClassName(className);
+	}else if(qString.indexOf("#")===0){
+		var idName = qString.substring(1,qString.length)
+		this.elements = [document.getElementById(idName)];
+	}
+}
+
+J.prototype.show = function(){}
+J.prototype.hide = function(){}
+J.prototype.html = function(value){}
+J.prototype.append = function(nodes){}
+J.ajax = function(option){}
+
+function CommService(){
+    this.rootURL = "https://communicatorai.herokuapp.com";
+	this.baseURL = this.rootURL + "/bot/3";
+	this.state = {
+		"action": "start"
+	};
+	this.initialized = false;
+	this.onreceive = function(){console.log("New state received");}
+}
+
+CommService.prototype._send = function(data){
+	var settings = {
+		"async": true,
+  		"url": this.baseURL,
+  		"method": "POST",
+  		"headers": {
+    		"Content-Type": "application/json"
+    	},
+    	"data": JSON.stringify(data)
+	};
+	var self = this;
+	$.ajax(settings).done(self.done);
+}
+
+CommService.prototype.init = function(){
+	if(this.initialized){
+		return;
+	}
+	var data ={
+		"state" :{
+			"action": "start"
+		}
+	};
+	this.initialized = true;
+	this._send(data);
+}
+CommService.prototype.send = function(msg){
+	if(!msg || msg.trim() === ""){
+		return;
+	}
+	var data ={
+		"input": msg,
+		"state" :this.state
+	};
+	this._send(data);
+}
+CommService.prototype._upload = function(file, signedRequest, url, callback){
+  const xhr = new XMLHttpRequest();
+  xhr.open('PUT', signedRequest);
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4){
+	if(xhr.status === 200){
+	    callback(url);
+      }
+      else{
+        alert('Could not upload file.');
+      }
+    }
+  };
+  xhr.send(file);
+}
+
+CommService.prototype._getSignedRequest = function(file,callback){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', this.rootURL + `/sign-s3?file-name=${file.name}&file-type=${file.type}`);
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4){
+      if(xhr.status === 200){
+        const response = JSON.parse(xhr.responseText);
+        this._upload(file, response.signedRequest, response.url,callback);
+      }
+      else{
+        alert('Could not get signed URL.');
+      }
+    }
+  };
+  xhr.send();
+}
+
+CommService.prototype.uploadFile = function(file,callback){
+    this._getSignedRequest(file,callback);
+}
+
+CommService.prototype.done = function(response){
+	if(!response){ console.error("Unable to get response from bot ");return; }
+	window.communicator.commservice.state = response.response.state;
+	var state = response.response.state;
+	window.communicator.chatrenderer.render(state.chatbox,state.options);
+}
+CommService.prototype.getState = function(){
+	return this.state;
+}
+
+
+var mediumBotHtml ='<div class="init card fadeIn animated bounce delay-1s shadow">'
+          + '<p ><strong style="padding-bottom:4px" >Hey Jobseeker</strong> 🖐, I am Jobeye AI <br>What sort of domain do you have experience in ?</p>'
+        	+'</div>'
+        +'<div class="init agentInput fadeIn animated bounce 800ms shadow">'
+        +'<input class="messageBox" type="text" name="" value="" placeholder="Type Message here">'
+        +'<div class="optionBox"></div>'
+    +'<div class="fileBox"><input type="file" name="file" id="file-input"></input></div>'
++ '<div class="uploadingBox">Uploading file..</div>'
+        +'<button class="sendBtn" type="button" name="button" >Send Message</button>'
+        +'</div>';
+
+var smallBotHtml = '<div class="communicatorChatBox shadow">'
+      + '<div class="communicatorAgent">'
+      +  '<div class="AgentIcon">'
+      +   '<svg xmlns="http://www.w3.org/2000/svg" width="51" height="51"><g data-name="Group 4"><g data-name="Group 3" transform="translate(-18.5 -45.5)" fill="#2680eb"><circle data-name="Ellipse 2" cx="25.5" cy="25.5" r="25.5" transform="translate(18.5 45.5)"/><path data-name="Path 2" d="M51.544 90.519l14.173 4.872a2.233 2.233 0 0 0 2.59-3.092l-6.233-14.138a1.97 1.97 0 0 0-3.364-.425l-7.94 9.265a2.2 2.2 0 0 0 .774 3.518z"/></g><path data-name="Path 1" d="M37.995 28.998a13.3 13.3 0 0 1-25.5 0" fill="none" stroke="#fff" stroke-linecap="round" stroke-miterlimit="10" stroke-width="5"/></g></svg>'
+      +    '<div class="chatClose">'
+      +        '<svg xmlns="http://www.w3.org/2000/svg" width="19" height="11"><path data-name="Path 3" stroke="#707070" d="M1.5 1.5l8 8 8-8" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" opacity=".27"/></svg>'
+      +   '</div>'
+      +  '</div>'
+      + mediumBotHtml
+      + '</div>'
+      +  '<div class="chatBody fadeIn animated bounce delay-1s ">'
+      + '<div class="aiReply">'
+      +    '<span class="chatTime">Jobeye Agent | 1:13 PM</span>'
+      +    '<p><strong style="padding-bottom:4px">Hey Jobseeker</strong> 🖐, I am Jobeye AI <br>What sort of domain do you have experience in ?</p>'
+      +  '</div>'
+      + '</div>'
+      + '<div class="branding fadeIn animated bounce delay-1s">⚡️ by Communicator AI</div>'
+      +'</div>';
+
+
+function ChatRenderer(){
+	this.rendered = {};
+}
+
+ChatRenderer.prototype.init = function(){
+    $(".fileBox").hide();
+}
+
+ChatRenderer.prototype.getChatDate = function(){
+	return (new Date()).toLocaleTimeString();
+}
+ChatRenderer.prototype.getBotChat= function(chat){
+	var div = document.createElement('div');
+	div.className = "aiReply";
+    var iH = '<span class="chatTime">Communicator Bot | '+this.getChatDate()+'</span>';
+    iH = iH + '<p >'+chat.message+'</p>';
+    div.innerHTML = iH;
+    return div;
+}
+ChatRenderer.prototype.getUserChat = function(chat){
+	var div = document.createElement('div');
+	div.className = "userReply";
+    var iH = '<span class="chatTime">You.  | '+this.getChatDate()+'</span>';
+    iH = iH + '<p >'+chat.message+'</p>';
+    div.innerHTML = iH;
+    return div;
+}
+ChatRenderer.prototype.appendBotText = function(chat){
+	if(chat.message !== ""){
+		$(".chatBody").append(this.getBotChat({"message":chat.message,"name":chat.author.name}));
+		var objDiv = $(".chatBody")[0];
+		objDiv.scrollTop = objDiv.scrollHeight;
+	}
+}
+ChatRenderer.prototype.appendUserText = function(chat){
+	if(chat.message !== ""){
+		$(".chatBody").append(this.getUserChat({"message":chat.message,"name":chat.author.name}));
+		$(".messageBox").val("");
+		var objDiv = $(".chatBody")[0];
+		objDiv.scrollTop = objDiv.scrollHeight;
+	}
+}
+ChatRenderer.prototype.appendChat = function(msg){
+	if(msg.author.name === "Jay"){
+		this.appendBotText(msg);
+	}else{
+		this.appendUserText(msg);
+	}
+}
+ChatRenderer.prototype.getOption = function(option){
+	l = document.createElement('li');
+	l.innerText = option;
+	l.setAttribute('value',option);
+    l.onclick = function(e){
+	$(".optionBox").html('');
+	// Clear options
+	window.communicator.commservice.send(e.target.getAttribute("value"));
+    }
+	return l;
+}
+
+function initFileBox(){
+    document.getElementById("file-input").onchange = () => {
+	const files = document.getElementById('file-input').files;
+	const file = files[0];
+	if(file == null){
+	    return alert('No file selected.');
+	}
+	// Show uploading symbol
+	$(".uploadingBox").show();
+	$(".fileBox").hide();
+	window.communicator.commservice.uploadFile(file,function(url){
+	    document.getElementById("file-input").onchange = () => {};
+	    $(".messageBox").val(url);
+	    $(".messageBox").show();
+	    // Hide uploading symbol
+	    window.communicator.commservice.send($(".messageBox").val().trim());
+	});
+  };
+
+}
+
+ChatRenderer.prototype.renderInput = function(options,msg){
+	$(".optionBox").hide();
+	$(".optionBox").html('')
+	$(".messageBox").show();
+	$(".fileBox").hide();
+	if(msg.type === 'file'){
+	    $(".messageBox").hide();
+	    $(".fileBox").show();
+	    initFileBox();
+	}else if(options && options.length>0){
+		$(".optionBox").html('');
+		var list = document.createElement('ul');
+		options.map((option)=>{
+			return this.getOption(option);
+		}).forEach((ch)=>{
+			list.append(ch);
+		})
+		$(".optionBox").append(list);
+		$(".optionBox").show();
+		$(".messageBox").hide();
+	}
+}
+ChatRenderer.prototype.render = function(chatbox,options){
+	chatbox.map((msg)=>{
+		return {
+			id: msg.timestamp,
+			msg: msg
+		}
+	}).filter((msg)=>{
+		return !this.rendered[msg.id]
+	}).forEach((msg)=>{
+		this.rendered[msg.id] = "1";
+		this.appendChat(msg.msg);
+	});
+	this.renderInput(options,chatbox[chatbox.length-1]);
+
+}
+
+function ChatSmallBox(){
+
+}
+
+ChatSmallBox.prototype.init = function(){
+    window.communicator.smallbox.render();
+    $(".fileBox").hide();
+    $(".uploadingBox").hide();
+    window.communicator.smallbox.setOnClick();
+}
+
+ChatSmallBox.prototype.setOnClick = function(){
+	$('.communicatorChatBox').on('click',function(){
+		$('.communicatorAgent').addClass('open');
+	});
+	$('.optionBox').hide();
+	$('.agentInput').click(function() {
+        $('.communicatorChatBox').addClass('expand');
+        $('.agentInput').removeClass('shadow');
+        $('.card').addClass('chat');
+        $('.communicatorAgent').addClass('expand');
+        $('.chatBody').css('display','block');
+        $('.card').css('display','none');
+        $('.chatClose').css('display','block');
+		window.communicator.commservice.init();
+    });
+	$(".sendBtn").click(function(){
+		window.communicator.commservice.send($(".messageBox").val().trim());
+	})
+	$(".messageBox").on('keydown',function(e){
+		if(e.key === "Enter"){
+			window.communicator.commservice.send($(".messageBox").val().trim());
+		}
+	})
+}
+
+ChatSmallBox.prototype.render = function() {
+	var di = document.createElement('div');
+	di.innerHTML = smallBotHtml;
+	document.getElementsByTagName('body')[0].appendChild(di.firstChild);
+};
+ChatSmallBox.prototype.show = function() {
+}
+ChatSmallBox.prototype.hide = function() {
+}
+
+function start(){
+	window.communicator = {};
+	window.communicator.smallbox = new ChatSmallBox();
+	window.communicator.smallbox.init();
+	window.communicator.chatrenderer = new ChatRenderer();
+    window.communicator.commservice = new CommService();
+
+}
+
+window.onload = start;
